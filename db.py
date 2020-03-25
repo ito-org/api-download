049af355b6349f2f096eaab62a5c7dabfbf0e408
haveyaseen <haveyaread@gmail.com>
2020-03-25 13:54:22 +0100

    fix db name

--- db.py
+++ db.py
@@ -3,7 +3,7 @@ from flask_pymongo import PyMongo
 import dateutil.parser
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb://172.31.16.148:27017/myDatabase"
+app.config["MONGO_URI"] = "mongodb://172.31.16.148:27017/bluto"
 mongo = PyMongo(app)
 
 
