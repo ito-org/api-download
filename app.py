2de3531726798569d36d59c8239aec1ecda82ef6
haveyaseen <haveyaread@gmail.com>
2020-03-26 16:24:49 +0100

    Add config files, allow all args to be optional (#7)
    * make all params in cases query optional
    
    * add config file for database connection

--- app.py
+++ app.py
@@ -1,38 +1,47 @@
 from flask import Flask, escape, request, jsonify, Response
-import dateutil
-import datetime
+from datetime import datetime
+from db import mongo, get_cases
+import dateutil.parser as dateparser
 
-from db import get_cases
+from typing import Any
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, instance_relative_config=True)
+env = os.environ.get("FLASK_ENV", "production")
+if env == "production":
+    app.config.from_object("config.ProductionConfig")
+elif env == "development":
+    app.config.from_object("config.DevelopmentConfig")
+elif env == "testing":
+    app.config.from_object("config.TestingConfig")
+
 
 app.config["APPLICATION_ROOT"] = "/"
 
+mongo.init_app(app)
 
 # TODO: instead of a timestamp passing a single UUID might be better here
 @app.route("/v1/cases")
 def cases():
     lat = request.args.get("lat", type=float)
     lon = request.args.get("lon", type=float)
-    if lat is None or lon is None:
-        return Response(None, status=400)
+    # TODO: use pid (latest pid that user had retrieved earlier) instead of since
+    pid = request.args.get("pid", type=str)
 
     try:
-        lat: int = round(lat)
-        lon: int = round(lon)
+        if lat is not None:
+            lat = round(lat)
+        if lon is not None:
+            lon = round(lon)
     except ValueError:
         return Response(None, status=400)
 
-    since = request.args.get("since")
-    if since is None:
-        # for querying the cases for the first time
-        cases = get_cases(lat, lon)
-    else:
+    since: Any = request.args.get("since", type=str)
+    if since is not None:
         try:
-            # for updating an existing local list of cases
-            since: datetime = dateutil.parser.isoparse(since)
-            cases = get_cases(lat, lon, since)
+            since = dateparser.isoparse(since)
         except ValueError:
             return Response(None, status=400)
 
+    cases = get_cases(lat=lat, lon=lon, since=since)
     return jsonify(cases)
--- db.py
+++ db.py
@@ -1,16 +1,17 @@
 from flask import Flask
 from flask_pymongo import PyMongo  # type: ignore
 from datetime import datetime
-from typing import Optional
+from typing import Optional, Dict, Any
 
-app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb://172.31.16.148:27017/bluto"
-mongo = PyMongo(app)
+mongo = PyMongo()
 
 
-def get_cases(lat: float, lon: float, since: Optional[datetime] = None):
-    if since is None:
-        result = mongo.db.cases.find({lat: lat, lon: lon})
-    else:
-        result = mongo.db.cases.find({lat: lat, lon: lon, since: {"$gte": since}})
-    return [case for case in result]
+def get_cases(lat: Optional[int], lon: Optional[int], since: Optional[datetime] = None):
+    conditions: Dict[str, Any] = {}
+    if lat is not None:
+        conditions["lat"] = lat
+    if lon is not None:
+        conditions["lon"] = lon
+    if since is not None:
+        conditions["since"] = {"$gte": since}
+    return [case for case in mongo.db.cases.find(conditions)]
